25a468e411de5869e776d5925dc38932ce56e163
Keisuke Sehara <keisuke.sehara@gmail.com>
2020-06-19 18:23:13 +0200

    fix issue with readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,6 +26,7 @@ author = 'Keisuke Sehara'
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
